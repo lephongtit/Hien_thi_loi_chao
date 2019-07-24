56a93a956205b03683037109c061331f7c32752c
Le Hoang Thien Phong <thienphong251199@gmail.com>
2019-07-24 09:02:06 +0700

    'oke'

---
+++ src/Hien_thi_loi_chao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hien_thi_loi_chao {
+    public static void main(String[] args) {
+        Scanner sr=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name= sr.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
